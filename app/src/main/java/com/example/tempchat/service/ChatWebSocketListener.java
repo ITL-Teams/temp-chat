ba656eb675d087a7574cf65796f42ae951054404
Angel Ricardo <angelricardo.dev@gmail.com>
2020-12-04 18:53:26 -0600

    Add: SocketUtils and Socket Listener

---
+++ app/src/main/java/com/example/tempchat/service/ChatWebSocketListener.java
@@ -0,0 +1,98 @@
+package com.example.tempchat.service;
+
+import android.os.Handler;
+
+import com.example.tempchat.MainActivity;
+import com.example.tempchat.model.ChatMessage;
+import com.example.tempchat.utils.Logger;
+import com.example.tempchat.utils.MessageFormatter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+
+public class ChatWebSocketListener extends WebSocketListener {
+  private Logger logger;
+  private MessageFormatter messageFormatter;
+  private Handler onOpenHandler,
+                  onMessageHandler,
+                  onCloseHandler,
+                  onFailureHandler;
+
+  public ChatWebSocketListener(
+          MainActivity context
+  ) {
+    messageFormatter = new MessageFormatter();
+    logger = new Logger(ChatWebSocketListener.class);
+  }
+
+  public void setOnOpenHandler(Handler onOpenHandler) {
+    this.onOpenHandler = onOpenHandler;
+  }
+
+  public void setOnMessageHandler(Handler onMessageHandler) {
+    this.onMessageHandler = onMessageHandler;
+  }
+
+  public void setOnCloseHandler(Handler onCloseHandler) {
+    this.onCloseHandler = onCloseHandler;
+  }
+
+  public void setOnFailureHandler(Handler onFailureHandler) {
+    this.onFailureHandler = onFailureHandler;
+  }
+
+  @Override
+  public void onOpen(WebSocket webSocket, Response response) {
+    String info = "Connected to server";
+    logger.info(info);
+    messageFormatter.putText(info);
+
+    if(onOpenHandler != null)
+      onOpenHandler.sendMessage(messageFormatter.getMessage());
+  }
+
+  @Override
+  public void onMessage(WebSocket webSocket, String data) {
+    logger.debug("data: " + data);
+
+    try {
+      messageFormatter.putChatMessage(
+        ChatMessage.fromJson(
+          new JSONObject(data)
+        )
+      );
+    } catch (JSONException jsonException) {
+      logger.error(jsonException.getMessage());
+    }
+
+    if(onMessageHandler != null)
+      onMessageHandler.sendMessage(messageFormatter.getMessage());
+  }
+
+  @Override
+  public void onClosed(WebSocket webSocket, int code, String reason) {
+    String info = "Connection closed";
+    logger.info("Connection closed");
+
+    messageFormatter.putText(info);
+
+    if(onCloseHandler != null)
+      onCloseHandler.sendMessage(messageFormatter.getMessage());
+  }
+
+  @Override
+  public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+    String info = t.getMessage();
+    logger.error(info);
+
+    messageFormatter.putText(info);
+
+    if(onFailureHandler != null)
+      onFailureHandler.sendMessage(messageFormatter.getMessage());
+  }
+
+}
---
+++ app/src/main/java/com/example/tempchat/service/SocketUtils.java
@@ -0,0 +1,65 @@
+package com.example.tempchat.service;
+
+import com.example.tempchat.model.ChatMessage;
+import com.example.tempchat.utils.GlobalConfig;
+import com.example.tempchat.utils.Logger;
+
+import org.json.JSONException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+
+public class SocketUtils {
+  private Logger logger;
+  private WebSocketListener listener;
+  private WebSocket ws;
+
+  public SocketUtils(WebSocketListener listener) {
+    logger = new Logger(SocketUtils.class);
+    this.listener = listener;
+  }
+
+  public void connect(String chatRoom) {
+    String serverUrl =
+            GlobalConfig.SOCKET_SCHEME + "://" +
+                    GlobalConfig.SERVER_ADDRESS + ":" +
+                    GlobalConfig.SERVER_PORT + "/chat/" +
+                    chatRoom;
+
+    Request request = new Request
+            .Builder()
+            .url(serverUrl)
+            .build();
+
+    OkHttpClient client = new OkHttpClient();
+    ws = client.newWebSocket(request, listener);
+    logger.info("Connecting with server...");
+  }
+
+  public void disconnect() {
+    if(ws == null) {
+      logger.info("Cannot disconnect: NULL Connection");
+      return;
+    }
+
+    ws.close(SocketStatus.NORMAL_CLOSURE_STATUS, SocketStatus.NORMAL_CLOSURE);
+    logger.info("Disconnected from server");
+  }
+
+  public void sendMessage(ChatMessage chatMessage) {
+    try {
+      ws.send(chatMessage.toJson().toString());
+    } catch (JSONException jsonException) {
+      logger.error("Disconnected from server: " + jsonException.getMessage());
+      ws.close(SocketStatus.CLOSE_GOING_AWAY, jsonException.getMessage());
+    }
+  }
+}
+
+class SocketStatus {
+  public static int NORMAL_CLOSURE_STATUS = 1000;
+  public static String NORMAL_CLOSURE = "NORMAL_CLOSURE";
+  public static int CLOSE_GOING_AWAY = 1001;
+}
